022edaf6e4745536cbd994cfed02e4da8215ef43
Sarath Saleem <sarath@one.com>
2015-08-01 12:37:44 +0400

    init files

---
+++ index.js
@@ -0,0 +1,29 @@
+var http = require('http'),
+    fs = require('fs'),
+    sys = require('sys'),
+    exec = require('child_process').exec;
+
+var child;
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
+    http.createServer(function(request, response) {
+        response.writeHeader(200, {"Content-Type": "text/html"});
+        response.write(html);
+        response.end();
+    }).listen(5000);
+
+
+    console.log('Server starts: 5000');
+
+
+    // or more concisely
+    function puts(error, stdout, stderr) { console.log(error, stdout, stderr) };
+
+    exec("ffmpeg -framerate 1/4 -i images/cat%03d.jpg -vcodec libx264 -t 30 -pix_fmt yuv420p videos/out.mp4", puts);
+
+
+});
+
