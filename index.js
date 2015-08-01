dbba79c14e47dbc3f915bff3cc75dcfc01354941
rab <rabish@one.com>
2015-08-01 16:38:06 +0400

    upload handler

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var app = express();
-
-app.get('/api/upload', function (req, res) {
-  res.send('Hello World!');
-});
+var upload = require('jquery-file-upload-middleware');
 
 app.use(express.static('http'));
+
+// upload
+app.use('/upload', function(req, res, next){
+    upload.fileHandler({
+        uploadDir: function () {
+            return __dirname + '/http/images/'
+        },
+        uploadUrl: function () {
+            return '/http/images/'
+        }
+    })(req, res, next);
+});
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
