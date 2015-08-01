2a6754028ecd080640bcfdb04208067444ff9ab8
Sarath Saleem <sarath@one.com>
2015-08-01 17:11:12 +0400

    Add createvideo

---
+++ createVideo.js
@@ -0,0 +1,26 @@
+var fs = require('fs'),
+    exec = require('child_process').exec;
+
+
+
+function createVideo() {
+
+    var that = this;
+
+    var inputImages = [  __dirname + '/http/images/cats/cat%03d.jpg'];
+    var output =  __dirname + "/http/videos/out.mp4";
+
+
+    // or more concisely
+    function puts(error, stdout, stderr) { console.log(error, stdout, stderr) };
+
+    var CMD = "ffmpeg -framerate 1/4 -i "+ inputImages +" -vcodec libx264 -t 30 -pix_fmt yuv420p " +  output;
+
+    console.log(CMD)
+
+    exec(CMD, puts);
+
+}
+
+module.exports = createVideo;
+
